1872d5848326c929f35e39df8800541b91476437
xieqing181 <33027473+xieqing181@users.noreply.github.com>
2017-11-09 21:40:51 +0800

    modify highs_low with input() to ask csv

--- highs_low.py
+++ highs_low.py
@@ -1,7 +1,36 @@
 import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
 
-filename = 'sitka_weather_2014.csv'
+#filename = '/Users/xieqing/Pythonwork/Chapter16/sitka_weather_2014.csv'
+filename = input("Please input csv file: ")
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    
+    dates, highs, lows = [], [], []
+    
+    for row in reader:
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+        
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
+plt.title("Daily high/low Temperature, 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
+plt.ylabel('Temperature (F)', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
